ea18829dc5c79b6f20ecebcfdbc9fa967919eff1
Gka2712 <g2172049@tcu.ac.jp>
2024-08-15 16:02:31 +0900

    database add

--- src/firebase.mjs
+++ src/firebase.mjs
@@ -1,12 +1,15 @@
 import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
 const firebaseConfig = {
-    apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+  apiKey:"AIzaSyCWVQggzErgJd5SpsfaLVxgfs08Ee-hkoA",
+  authDomain:"react-auth-fb2dc.firebaseapp.com",
+  projectId:"react-auth-fb2dc",
+  storageBucket:"react-auth-fb2dc.appspot.com",
+  messagingSenderId:"905415073514",
+  appId: "905415073514"
   };
 const app=initializeApp(firebaseConfig);
-export const auth=getAuth(app);
+const auth=getAuth(app);
+const db=getFirestore(app);
+export {auth,db};
--- src/home.js
+++ src/home.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { auth } from './firebase';
+import React, { useState,useEffect } from 'react';
+import { db,auth } from './firebase';
 import { signOut } from 'firebase/auth';
+import {collection,getDocs} from 'firebase/firestore';
 import { useNavigate,Navigate } from 'react-router-dom';
 import { useAuthContext } from './authcontext';
 
 const Home = () => {
+  console.log(db);
   const navigate = useNavigate();
   const { user } = useAuthContext();
-
+  const [data,setData]=useState([]);
   const handleLogout = () => {
     signOut(auth)
       .then(() => {
@@ -18,7 +20,32 @@ const Home = () => {
         console.log("ログアウトに失敗しました。", error);
       });
   };
-
+  const formatDate=(timestamp)=>{
+    const date=new Date(timestamp.seconds*1000);
+    const month=('0'+(date.getMonth()+1)).slice(-2);
+    const day=('0'+date.getDate()).slice(-2);
+    const hours=('0'+date.getHours()).slice(-2);
+    const minutes=('0'+date.getMinutes()).slice(-2);
+    const seconds=('0'+date.getSeconds()).slice(-2);
+    return `${month}/${day} ${hours}:${minutes}:${seconds} `;
+  };
+  useEffect(()=>{
+    const fetchData=async()=>{
+      if(user){
+        try{
+          const querySnapshot=await getDocs(collection(db,"event"));
+          const items=querySnapshot.docs.map(doc=>({
+            id:doc.id,
+            ...doc.data()
+          }));
+          setData(items);
+        }catch(error){
+          console.error("データの取得に失敗しました",error);
+        }
+      }
+    };
+    fetchData();
+  },[user]);
   if (!user) {
     return <Navigate to="/login" />;
   } else {
@@ -26,6 +53,23 @@ const Home = () => {
       <div>
         <h1>ホームページ</h1>
         <button onClick={handleLogout}>ログアウト</button>
+        <h2>イベント</h2>
+        <table border="1">
+          <thead>
+            <th>イベント</th>
+            <th>開催日時</th>
+            <th>場所</th>
+          </thead>
+          <tbody>
+            {data.map(item=>(
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{formatDate(item.day)}</td>
+                <td>{item.place}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
